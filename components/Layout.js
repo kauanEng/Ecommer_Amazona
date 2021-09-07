65c44746e236eb29ce68ed92e739204a730ff97d
Kauan Santos <kauansantos83@gmail.com>
2021-09-07 15:03:25 -0300

    adicionado modo dark

---
+++ components/Layout.js
@@ -0,0 +1,90 @@
+import React, { children, useContext } from 'react';
+import Head from 'next/head';
+import { Store } from '../utils/Store';
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Link,
+  createMuiTheme,
+  ThemeProvider,
+  CssBaseline,
+  Switch,
+} from '@material-ui/core';
+import useStyles from '../utils/stylesLayout';
+import NextLink from 'next/link';
+import Cookies from 'js-cookie';
+
+export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: '50',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    typography: {
+      h2: {
+        fontSize: '1.1rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      second: {
+        main: '#208080',
+      },
+    },
+  });
+  const classes = useStyles();
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
+  return (
+    <div>
+      <Head>
+        <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
+        {description && <meta name="description" content={description}></meta>}
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>amazona</Typography>
+              </Link>
+            </NextLink>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography> Todos os direitos reservados. Next Amazona.</Typography>
+        </footer>
+      </ThemeProvider>
+    </div>
+  );
+}
---
+++ utils/stylesLayout.js
@@ -0,0 +1,32 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  navbar: {
+    backgroundColor: '#203040',
+    '& a': {
+      colors: '#ffffff',
+      marginLeft: 10,
+    },
+  },
+  brand: {
+    fontWeight: 'bold',
+    fontSize: '1.5rem',
+    color: '#ffffff',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+
+  main: {
+    minHeight: '80vh',
+  },
+  footer: {
+    textAlign: 'center',
+  },
+  section: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+});
+
+export default useStyles;
